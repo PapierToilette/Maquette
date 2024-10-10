77e721bfd16aee3762257d006995017904ebf26f
Evan <evan.beaufreton@gmail.com>
2024-10-10 03:28:02 +0200

    fix js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const buttonP1_1 = document.getElementById('buttonP1_1');
-const buttonP1_2 = document.getElementById('buttonP1_2');
-const buttonP1_3 = document.getElementById('buttonP1_3');
-const buttonP1_4 = document.getElementById('buttonP1_4');
+const buttons = [
+    document.getElementById('buttonP1_1'),
+    document.getElementById('buttonP1_2'),
+    document.getElementById('buttonP1_3'),
+    document.getElementById('buttonP1_4')
+];
 
 const texte_background =  document.getElementById('texte_background');
 
@@ -11,71 +13,6 @@ function changeBackgroundImage(newImageUrl) {
     page1.style.backgroundImage = `url(${newImageUrl})`;
 }
 
-/* CODE PAS OPTIMISÉ
-
-const txtP1_1 = "ÉVOLUER, PERFORMER, <br> TRANSFORMER, <br> PENSER EN ATHLÈTE!";
-const txtP1_2 = "IDENTIFIER <br> LE BON TALENT";
-const txtP1_3 = "ASSURER <br> LA TRANSITION";
-const txtP1_4 = "FACILITER <br> LE LEADERSHIP <br> DEVELOPMENT";
-const txtP1_5 = "ACCÉLÉRER <br> LA TRANSFORMATION <br> DIGITALE & RSE";
-
-let reset1 = 0;
-let reset2 = 0;
-let reset3 = 0;
-let reset4 = 0;
-
-buttonP1_1.addEventListener('click', function() {
-    if (reset1 == 1) {
-        texte_background.innerHTML = txtP1_1;
-        reset1 = 0;
-    } else {
-        texte_background.innerHTML = txtP1_2;
-        reset1 = 1;
-        reset2 = 0;
-        reset3 = 0;
-        reset4 = 0;
-    } 
-});
-buttonP1_2.addEventListener('click', function() {
-    if (reset2 == 1) {
-        texte_background.innerHTML = txtP1_1;
-        reset2 = 0;
-    } else {
-        texte_background.innerHTML = txtP1_3;
-        reset1 = 0;
-        reset2 = 1;
-        reset3 = 0;
-        reset4 = 0;
-    }
-});
-buttonP1_3.addEventListener('click', function() {
-    if (reset3 == 1) {
-        texte_background.innerHTML = txtP1_1;
-        reset3 = 0;
-    } else {
-        texte_background.innerHTML = txtP1_4;
-        reset1 = 0;
-        reset2 = 0;
-        reset3 = 1;
-        reset4 = 0;
-    }
-});
-buttonP1_4.addEventListener('click', function() {
-    if (reset4 == 1) {
-        texte_background.innerHTML = txtP1_1;
-        reset4 = 0;
-    } else {
-        texte_background.innerHTML = txtP1_5;
-        reset1 = 0;
-        reset2 = 0;
-        reset3 = 0;
-        reset4 = 1;
-    }
-});
-
-*/
-
-/* Merci chatgpt */
 const texts = [
     "ÉVOLUER, PERFORMER, <br> TRANSFORMER, <br> PENSER EN ATHLÈTE!", // index 0
     "IDENTIFIER <br> LE BON TALENT",                                 // index 1
@@ -92,17 +29,27 @@ const imgs = [
     'img/fond5.jpg'
 ];
 
+const colors = [
+    'red',
+    'cyan',
+    'green',
+    'yellow'
+];
+
 let txt_index = 0;
 
 const handleButtonClick = (index) => {
     if (txt_index === index) {
+        buttons[index].style.backgroundColor('white');
         texte_background.innerHTML = texts[0];
         changeBackgroundImage(imgs[0]); 
         txt_index = 0;
     } else {
+        buttons[index].style.backgroundColor('white');
         texte_background.innerHTML = texts[index];
         changeBackgroundImage(imgs[index]); 
         txt_index = index;
+        buttons[index].style.backgroundColor(colros[index]);
     }
 };
 
